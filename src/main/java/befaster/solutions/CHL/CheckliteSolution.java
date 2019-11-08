a0517cd845df4393ce0861f886ea0f21a48bb1ef
sbx_user1051 <sbx_user1051@169.254.164.189>
2019-11-08 04:34:04 +0000

    Fri Nov 08 04:34:04 UTC 2019

--- src/main/java/befaster/solutions/CHL/CheckliteSolution.java
+++ src/main/java/befaster/solutions/CHL/CheckliteSolution.java
@@ -4,6 +4,12 @@ import befaster.runner.SolutionNotImplementedException;
 
 public class CheckliteSolution {
     public Integer checklite(String skus) {
-        throw new SolutionNotImplementedException();
+        //separate number and item name
+
+        //offer count%total number = original item to price
+        //offer count/total number = offer price
+
+        // get value of item from map
     }
 }
+
